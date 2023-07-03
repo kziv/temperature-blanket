f68f5b354aee803beb16aeb46355dfee3081dc5a
Karen Ziv <me@karenziv.com>
2023-07-03 08:39:36 -0400

    Added cors module

--- app/app.js
+++ app/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 3000;
 
 // Load routes.
 require('./src/routes')(app);
+
+// Use CORS and temporarily allow all origins for local testing.
+app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
+});
 
 if (!module.parent) {
   const server = app.listen(port);
