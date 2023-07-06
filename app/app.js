898edd6fb0aa8a5454f59b10dcbc921e23bff709
Karen Ziv <me@karenziv.com>
2023-07-06 10:11:49 -0400

    - Removed cors and related code since api is on the same host now - Moved some text into local vars

--- app/app.js
+++ app/app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const exphbs = require('express-handlebars'); // View templates
 const path = require('path');
-const cors = require('cors');
 
 // Initialize express framework.
 const app = express();
@@ -10,23 +9,22 @@ const app = express();
 // Load routes.
 require('./src/routes')(app);
 
-// Use CORS and temporarily allow all origins for local testing.
-app.use(cors());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  next();
-});
-
 // View engine
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 app.set('views', './src/views');
 
+// ----- Local vars -----
+// If this gets unwieldy, move to a separate file.
+app.locals.siteTitle = 'Historical Temperature Blanket Generator';
+app.locals.currentYear = new Date().getFullYear();
+// ----------------------
+
 // ----- Middleware -----
 app.use(express.static(path.join(__dirname, 'public'))); // Static files.
 // ----------------------
 
+// Spool up the app.
 if (!module.parent) {
   const server = app.listen(process.env.PORT);
   // Set server timeout for 10 minutes.
